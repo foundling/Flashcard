bec2eb280935a386d379a5faa0d61a5f92d17a1a
Alex Ramsdell <alexramsdell@gmail.com>
2015-03-10 20:03:01 -0400

    fixed some readability issues

--- __init__.py
+++ __init__.py
@@ -13,51 +13,66 @@ PARSE_FILES_DIR = APP_DATA_DIR + 'parse_files/'
 DB_PATH = APP_PATH + '/db'
 DB_DIR = DB_PATH + '/'
 
-
 def main_menu(CURRENT_CARD_SET = None):
   clear_screen()
 
   prompt('Please choose an Option from the Menu',False)
+
   if CURRENT_CARD_SET:
-    prompt('current card set: %s\n' % (CURRENT_CARD_SET),False)
+    prompt('current card set: %s\n' % (CURRENT_CARD_SET), False) # False means no return value
+
+  #  Please choose an Option from the Menu
+  #
+  #  (1) Create a New Card Set
+  #  (2) Load Card Set
+  #  (3) Add a Card to an Existing Card Set
+  #  (4) Quiz yourself
+  #  (5) Quit
+
   choices = ['Create a New Card Set', 'Load Card Set', 'Add a Card to an Existing Card Set', 'Quiz yourself', 'Quit']
-  print '\n'.join("({}) {}".format(n,v) for n,v in enumerate(choices,start=1))
+  print '\n'.join("({}) {}".format(n,v) for n,v in enumerate(choices, start=1))
   response = prompt('',True)
 
   if response in ['1']:
     create_new_card_set() 
+
   if response in ['2']:
     card_set = load_card_set()
     return card_set
+
   if response in ['3']:
     add_to_existing_card_set()
+
   if response in ['3']:
     quiz_yourself()
+
   if response in ['q','Q','5']:
     sys.exit(0)
   
 def create_new_card_set(error_msg=None):
+
   clear_screen()
  
   if error_msg:
-    name = clean_filename(prompt(error_msg))
+    name = clean_filename( prompt(error_msg) )
+
   else:
-    name = clean_filename(prompt('Please Enter a Name for your new Flashcard Set:  '))
+    name = clean_filename( prompt('Please Enter a Name for your new Flashcard Set:  ') )
 
   if name:
-
     if file_exists(name):
       error_msg = 'This file already exists. Please Choose Another Name:\n'
       create_new_card_set(error_msg)
 
     else:
-
       card_set_name = DB_DIR + name + '.db'
       with open(card_set_name, 'a+') as f:
         try:
           f.write('') # create file
+
         except IOError:
           print "Could Not Open The Requested Card Set. See Log for Details"
+
         else: 
           db = Database(DB_DIR + name + '.db')
           response = prompt('Card Set %s created successfully. Do you want to add cards to your cardset now? [y/N]' % (name))
@@ -76,36 +91,48 @@ def create_new_card_set(error_msg=None):
 
 # ACTIVATE CARD SET
 def load_card_set():
+
   clear_screen()
   cardsets = [ (DB_DIR + f) for f in os.listdir(DB_PATH) if f.endswith('.db') ]
   cardsets.sort(key=os.path.getctime)
+
   if cardsets:
     prompt('Please Choose a Cardset:', False)
     print '\n'.join('({}) {}'.format(n,os.path.basename(v)) for n,v in enumerate(cardsets))  
     response = int(prompt())
     db = Database(cardsets[response])
+
     if db.db_name:
       CURRENT_CARD_SET = db.db_name
       prompt('Card Set Loaded!')
       return CURRENT_CARD_SET
+
   else:
     prompt('You have no Card Sets. Hit any Key to return to the main Menu')
 
-# PUT NEW CARDS INTO DB
+# PUT NEW CARDS INTO DB, ONE BY ONE
 def load_cards_manually(db):
+
   clear_screen() 
   front,back = None, None
+
   while True:
     prompt("Enter 'q' or 'Q' into either field to quit at any time",False)
     front = prompt("FRONT: ")
+
     if front in ['q','Q']:
       break
+
     back = prompt("BACK: ")
+
     if back in ['q','Q']:
       break
+
     card = [front, back]
     db.add_card_to_set(card)
 
+
+# PARSE A FILE INTO CARDS
 def load_cards_from_file(db):
   clear_screen() 
   prompt('[ NOTE: Files to be parsed should be placed in %s ]' % (PARSE_FILES_DIR),False)
@@ -138,8 +165,10 @@ def clear_screen():
 def file_exists(filename=None, loc=None):
   if loc is None:
     loc = APP_PATH + '/db'
+
   if filename:
     return os.path.isfile(loc + '/' + filename)
+
   else:
     print 'no filename passed' 
     return None
@@ -150,9 +179,12 @@ def perror(error=None):
 
 # prompts or just prints if you pass it prompt = False
 def prompt(text='',response=True):
+
   text = text.strip()
+
   if response:
     return raw_input(text)
+
   else:
     print text + '\n'
 
@@ -160,9 +192,12 @@ def handle(response):
   pass
 
 def main():
+
   card_set = None
+
   while True:
     card_set = main_menu(CURRENT_CARD_SET=card_set)
+
     if card_set:
       card_set = os.path.basename(card_set).split('.db')[0].upper()
     
