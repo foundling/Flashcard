911ea49108922e9301201122ff8f14a885cbd9de
Alex Ramsdell <alexramsdell@gmail.com>
2015-03-13 16:49:48 -0400

    some reformatting of helper_funcs

--- helper_funcs.py
+++ helper_funcs.py
@@ -2,21 +2,31 @@ import os
 from config import *
 
 def show_available_files(filepath, extension=''):
-  files = [ (os.path.dirname(filepath) + '/' + f) for f in os.listdir(filepath) if f.endswith(extension)]
+  '''
+  documentation to be written
+  '''
+  files = [ (os.path.dirname(filepath) + '/' + f) for f in os.listdir(filepath)\
+    if f.endswith(extension) and not f.startswith('.')]
   #for n, v in enumerate(files, start=1):
   #  print "({}) {}".format(n,os.path.basename(v))
   return files
    
 def clean_filename(filename):
+  '''
+
+  '''
   return filename.replace(' ','_')
 
-def add_to_existing_card_set(db):
-  pass
-
 def clear_screen():
+  '''
+
+  '''
   os.system('clear')
 
 def file_exists(filename=None, loc=None):
+  '''
+
+  '''
   if loc is None:
     loc = DB_PATH
 
@@ -28,10 +38,16 @@ def file_exists(filename=None, loc=None):
     return None
 
 def perror(error=None):
+  '''
+
+  '''
   if error: print error
 
 # prompts or just prints if you pass it prompt = False
 def prompt(text='', response=True, leading_newlines = 0, trailing_newlines = 0):
+  '''
+
+  '''
 
 ## add some logic in here to limit line lenght of text, inserting new lines at '.' before 80 char.
 
